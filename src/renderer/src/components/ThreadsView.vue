ba99afa222210facb164b417f8b3042ad01bbada
cyl <2893176150@qq.com>
2025-04-08 18:00:00 +0800

    feat: 初始化threadsview

--- src/renderer/src/components/ThreadsView.vue
+++ src/renderer/src/components/ThreadsView.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="w-60 h-full bg-muted overflow-y-auto p-2 space-y-3 border-r"></div>
+  <div class="w-60 h-full bg-muted overflow-y-auto p-2 space-y-3 border-r">
+    <div>
+      <a-button
+        variant="outline"
+        size="small"
+        class="w-full text-xs text-muted-foreground justify-start gap-2"
+      >
+        <Icon icon="lucide:pen-line" class="h-4 w-4" /> 新会话
+      </a-button>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
 <script lang="ts">
 export default {
   name: 'ThreadsView'
--- src/renderer/src/views/ChatTabView.vue
+++ src/renderer/src/views/ChatTabView.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="flex h-full bg-background">
+    <Transition
+      enter-active-class="transition-all duration-300 ease-out"
+      leave-active-class="transition-all duration-300 ease-in"
+      enter-from-class="-translate-x-full opacity-0"
+      leave-to-class="-translate-x-full opacity-0"
+    >
+      <!-- TODO  -->
+      <ThreadsView class="transform" />
+    </Transition>
     ChatTabView
 
     <div class="flex flex-1 flex-col">回话</div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ThreadsView from '@/components/ThreadsView.vue'
+</script>
 <script lang="ts">
 export default {
   name: 'ChatTabView'
